2b598611687f75bc18775785b0925f5d86ced03d
huliangze <huliangze@xiaomi.com>
2017-10-29 16:58:21 +0800

    implement hello world page

---
+++ src/main/java/com/catlife/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.catlife.controllers;
+
+import net.paoding.rose.web.annotation.Path;
+import net.paoding.rose.web.annotation.rest.Get;
+
+/**
+ * Created by Wholanz on 2017/10/29.
+ */
+@Path("/")
+public class TestController {
+    @Get("test")
+    public String test() {
+        return "@HelloWorld!";
+    }
+}
